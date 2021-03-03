5d13f3ebf046321b6cf06da9f8fde60087830c93
nguyenttaibi <56423018+nguyenttaibi@users.noreply.github.com>
2021-03-03 14:46:19 +0700

    cham soc nguoi nha nam vien chua the lam login remember

--- PHP/04-20210301/BT/index.php
+++ PHP/04-20210301/BT/index.php
@@ -12,22 +12,22 @@ if (preg_match($checkName, $_POST['name'])){
     echo "exit";
 }
 
-$checkName = '^[a-z,-]+[a-z,-]+$';
-if (preg_match($checkName, $_POST['name'])){
+$checkEmail = '/^\S*[a-zA-Z0-9-_]+@[a-z]{5}\.[a-z]{2}\S*$/i';
+if (preg_match($checkEmail, $_POST['email'])){
     echo "ok";
 }else{
     echo "exit";
 }
 
-$checkName = '^[a-z,-]+[a-z,-]+$';
-if (preg_match($checkName, $_POST['name'])){
+$checkpw = '/^\S*(?=\S{8,})(?=\S*[a-z])(?=\S*[A-Z])(?=\S*[\d])\S*$/';
+if (preg_match($checkpw, $_POST['password'])){
     echo "ok";
 }else{
     echo "exit";
 }
 
-$checkName = '^[a-z,-]+[a-z,-]+$';
-if (preg_match($checkName, $_POST['name'])){
+$checkcpw = '^[a-z,-]+[a-z,-]+$';
+if (preg_match($checkcpw, $_POST['cfpassword'])){
     echo "ok";
 }else{
     echo "exit";
